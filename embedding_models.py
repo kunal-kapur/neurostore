a8068e461935c72e549858ed4ef83f09bae1bba5
Kunal Kapur <kunalkapur@Kunals-MacBook-Air.local>
2024-08-15 18:24:03 -0700

    support for other embedding models

--- embedding_models.py
+++ embedding_models.py
@@ -1,10 +1,11 @@
 from typing import Any, Literal, Mapping
 from pymilvus import model
 from abc import abstractmethod
+from openai import OpenAI
 
 
 
-EmbeddingCompletion = Literal["Default", "paraphrase-albert-small-v2"] 
+EmbeddingCompletion = Literal["Default", "paraphrase-albert-small-v2", "text-embedding-3-small"] 
 
 class EmbeddingModel:
     @property
@@ -32,11 +33,38 @@ class AlbertSmalllV2(EmbeddingModel):
     def __call__(self, words: list[str]) -> Any:
         return self.embedding_model(words)
     
+class OpenAI_Small(EmbeddingModel):
+    def __init__(self) -> None:
+        self.embedding_model = OpenAI()
+    @property
+    def dimension(self):
+        return 1536
+    @property
+    def name(self):
+        return "text-embedding-3-small"
+
+    def __call__(self, words: list[str]) -> Any:
+        return self.embedding_model.embeddings.create(input=words, model=self.name)
+    
+class OpenAI_Large(EmbeddingModel):
+    def __init__(self) -> None:
+        self.embedding_model = OpenAI()
+    @property
+    def dimension(self):
+        return 3072
+    @property
+    def name(self):
+        return "text-embedding-3-large"
+
+    def __call__(self, words: list[str]) -> Any:
+        return self.embedding_model.embeddings.create(input=words, model=self.name)
+    
 
 
 
 EmbeddingCompletionMapping: Mapping[str, EmbeddingModel] = {
     "Default": AlbertSmalllV2,
-    "paraphrase-albert-small-v2": AlbertSmalllV2
-    
+    "paraphrase-albert-small-v2": AlbertSmalllV2,
+    "text-embedding-3-small": OpenAI_Small,
+    "text-embedding-3-large": OpenAI_Large
 }
